2d987005693ebdfef9aa2bfea9858c0d68703dbd
Milton Young <miltonyoung911@gmail.com>
2026-09-22 00:42:25 -0700

    Set MaxBatchSize to 3 for testing

--- hybrid-gaming-workstation/photoshop-watcher/Watcher.ps1
+++ hybrid-gaming-workstation/photoshop-watcher/Watcher.ps1
@@ -3,7 +3,7 @@
 # Processes images in batches of $MaxBatchSize, then restarts Photoshop to release memory.
 
 $RootFolder = "E:\CreativeBridge\photoshop-projects"
-$MaxBatchSize = 10
+$MaxBatchSize = 3
 $PollIntervalSeconds = 30
 $PhotoshopExe = "C:\Program Files\Adobe\Adobe Photoshop 2026\Photoshop.exe"
 $RunBatchTemplate = "C:\Users\milton\projects\opencode-personal-assistant-projects\hybrid-gaming-workstation\photoshop-watcher\RunBatch.jsx.template"
